ff369204b63bc44f6d168bfe9621d3bebd7c75fb
Adam Kapelner <kapelner@gmail.com>
2014-01-28 18:25:15 -0500

    made raw data file nicer

--- R_analysis/basic.R
+++ R_analysis/basic.R
@@ -1,9 +1,5 @@
 #assume data is loaded
 
-#pull out X and y
-y = X[, 1]
-ybin = ifelse(y > 0, 1, 0)
-
 X = as.matrix(X[, 2 : ncol(X)])
 Xsub = X[, 1 : 100]
 
@@ -11,45 +7,45 @@ Xsub = X[, 1 : 100]
 #run some models
 
 
-mod = lm(y ~ Xsub)
+mod = lm(y_diff ~ Xsub)
 summary(mod)
 
 Xsub = X[, 1 : 1000]
 
-mod = lm(y ~ Xsub)
+mod = lm(y_diff ~ Xsub)
 summary(mod)
 
 library(glmnet)
 
 
-mod = cv.glmnet(Xsub, y, alpha = 0)
+mod = cv.glmnet(Xsub, y_diff, alpha = 0)
 vars = coef(mod)
 vars = which(vars != 0) - 1
 vars
 
 
-Xsuby = cbind(X, y)
-Xsuby = Xsuby[order(y), ]
-length(y)
+Xsuby = cbind(X, y_diff)
+Xsuby = Xsuby[order(y_diff), ]
+length(y_diff)
 dim(Xsuby)
 Xsuby = Xsuby[1000 : 3000, ]
 Xsub = Xsuby[, 1 : (ncol(Xsuby) - 1)]
-y = Xsuby[, ncol(Xsuby)]
+y_diff = Xsuby[, ncol(Xsuby)]
 
 
 Xsub = Xsub[, 1 : 1000]
 
-mod = lm(y ~ Xsub)
+mod = lm(y_diff ~ Xsub)
 summary(mod)
 
 #lasso
-mod = cv.glmnet(Xsub, y, alpha = 1)
+mod = cv.glmnet(Xsub, y_diff, alpha = 1)
 vars = coef(mod)
 vars = which(vars != 0) - 1
 vars
 
 #ridge
-mod = cv.glmnet(Xsub, y, alpha = 1)
+mod = cv.glmnet(Xsub, y_diff, alpha = 1)
 vars = coef(mod)
 vars = which(vars != 0) - 1
 vars
