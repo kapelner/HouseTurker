fb49f9d94e79fcc5dcc039c1be5310ac4341d1af
Benson Chen <bensonc@wharton.upenn.edu>
2014-01-28 17:57:24 -0500

    printed out last 300 words

--- src/HouseTurkerLoader/WordDictionary.java
+++ src/HouseTurkerLoader/WordDictionary.java
@@ -9,48 +9,54 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class WordDictionary {
-	private LinkedHashMap<String, Integer> dict;
-	private String word;
-	private int counter;
+  private LinkedHashMap<String, Integer> dict;
+  private String word;
+  private int counter;
 
-	public WordDictionary(String inputfile) throws IOException {
-		dict = new LinkedHashMap<String, Integer>();
-		WordScanner s = new WordScanner(new FileReader(inputfile));
-		add(s);
-	}
+  public WordDictionary(String inputfile) throws IOException {
+    dict = new LinkedHashMap<String, Integer>();
+    WordScanner s = new WordScanner(new FileReader(inputfile));
+    add(s);
+  }
 
-	private void add(WordScanner s) {
-		while (s.hasNext() && s != null) {
-			word = s.next();
-			if (!dict.containsKey(word) && word != "" && word != " ") {
-				dict.put(word, new Integer(counter));
-				counter++;
-			}
-		}
-	}
-	
-	public void printString() {
-		for (Map.Entry<String, Integer> entry: dict.entrySet()) {
-			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-		}
-	}
-	
-	public int getSize() {
-		return dict.size();
-	}
+  private void add(WordScanner s) {
+    while (s.hasNext() && s != null) {
+      word = s.next();
+      if (!dict.containsKey(word) && word != "" && word != " ") {
+        dict.put(word, new Integer(counter));
+        counter++;
+      }
+    }
+  }
 
-	public int getValue(String word) {
-		return dict.get(word);
-	}
+  public void printString() {
+    for (Map.Entry<String, Integer> entry : dict.entrySet()) {
+      System.out.println("Key = " + entry.getKey() + ", Value = "
+          + entry.getValue());
+    }
+  }
 
-	public boolean contains(String w) {
-		return dict.containsKey(w);
-	}
-	
-	public void write() throws FileNotFoundException, UnsupportedEncodingException {
-		PrintWriter writer = new PrintWriter("dictmap.txt", "UTF-8");
-		for (Map.Entry<String, Integer> entry: dict.entrySet()) {
-			writer.write("Key = " + entry.getKey() + ", Value = " + entry.getValue() + "\n");
-		}
-	}
+  public int getSize() {
+    return dict.size();
+  }
+
+  public int getValue(String word) {
+    return dict.get(word);
+  }
+
+  public boolean contains(String w) {
+    return dict.containsKey(w);
+  }
+
+  public void write() throws FileNotFoundException,
+      UnsupportedEncodingException {
+    PrintWriter writer = new PrintWriter("dictmap.txt", "UTF-8");
+    for (Map.Entry<String, Integer> entry : dict.entrySet()) {
+      if (entry.getValue() > 8930) {
+        writer.write("Key = " + entry.getKey() + ", Value = "
+            + entry.getValue() + "\n");
+      }
+    }
+    writer.close();
+  }
 }
