29a2c0a13a8e48c61afdd4fd3c071c394d79fc1c
Adam Kapelner <kapelner@gmail.com>
2014-03-19 16:22:02 -0400

    load raw data

--- R_analysis_abs_raw/load_data.R
+++ R_analysis_abs_raw/load_data.R
@@ -1,14 +1,18 @@
 X = read.table("data_f.txt", sep = " ")
-
-word_names = read.csv("dictmap2.csv", header = FALSE)
+X = X[, 1 : 9296]
+X = cbind(abs(X[, 1]), X)
+word_names = read.csv("dictmap.csv", header = FALSE)
 word_names = sapply(word_names, as.character) #convert to characters
 colnames(X) = c("y_diff", "y_diff_turk", "y_ratio", "y_ratio_turk", word_names[1 : 9293])
-rm(word_names)
-
 
 #pull out X and y
 y_diff = X[, 1]
-X = X[, 2 : ncol(X)]
+y_diff_turk = X[, 2]
+
+y_diff_turk_bin = ifelse(y_diff_turk > 0, 1, 0)
+y_ratio = X[, 3]
+y_ratio_turk = X[, 4]
+
 
 #checks
 sum(y_diff_turk > 0) / length(y_diff_turk)
